a8f6725fc51f54b91ddf5e6371d508aee4efaf06
Kirill Kurochkin <kirillkurochkinm@gmail.com>
2025-07-29 21:16:42 +0600

    Применяем Pydantic в AuthenticationClient

--- clients/exercises/exercises_client.py
+++ clients/exercises/exercises_client.py
@@ -3,7 +3,7 @@ from typing import TypedDict
 from httpx import Response
 
 from clients.api_client import APIClient
-from clients.private_http_builder import AuthenticationUserDict, get_private_http_client
+from clients.private_http_builder import get_private_http_client, AuthenticationUserSchema
 
 
 class GetExercisesQueryDict(TypedDict):
@@ -133,7 +133,7 @@ class ExercisesClient(APIClient):
         return self.delete(f"/api/v1/exercises/{exercise_id}")
 
 
-def get_exercises_client(user: AuthenticationUserDict) -> ExercisesClient:
+def get_exercises_client(user: AuthenticationUserSchema) -> ExercisesClient:
     """
     Функция создаёт экземпляр ExercisesClient с уже настроенным HTTP-клиентом.
 
